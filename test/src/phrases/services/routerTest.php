11c74deadf0faf88666102cad3a30b766c8f2208
malukenho <admin@phpse.net>
2013-09-24 16:13:40 -0300

    Create tests for method setURI of class Router
    - Create test to certify if the method setURI of class \Phrases\Router can storage the values passed correctly
    - Create a depends realtion between the testClassRouterCanStorageURICorrectly  and testRouterClassCanReturnSlugRequiredCorrectly tests

--- test/src/phrases/services/routerTest.php
+++ test/src/phrases/services/routerTest.php
@@ -4,7 +4,7 @@ namespace Phrases\Services;
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function provider() 
+    public function providerURIDataAndExpectedResult() 
     {
         return array(
             array('phrases/quote', 'false'),
@@ -16,7 +16,29 @@ class RouterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider provider
+     * @dataProvider providerURIDataAndExpectedResult
+     */
+    public function testClassRouterCanStorageURICorrectly($url)
+    {
+        $router = new Router;
+
+        $reflection = new \ReflectionClass(get_class($router));
+
+        $this->assertTrue($reflection->hasProperty('uri'), 
+            'Class has not a property called "uri"');
+
+        $router->setURI($url);
+
+        $uriProperty = $reflection->getProperty('uri');
+        $uriProperty->setAccessible(true);
+
+        $this->assertEquals($uriProperty->getValue($router), $url, 'Ooops! The value is not correctly storaged in uri property on class ' . get_class($router));
+
+    }
+
+    /**
+     * @depends testClassRouterCanStorageURICorrectly
+     * @dataProvider providerURIDataAndExpectedResult
      */
     public function testRouterClassCanReturnSlugRequiredCorrectly($url, $expected)
     {
@@ -25,10 +47,9 @@ class RouterTest extends \PHPUnit_Framework_TestCase
         $slug   =   $router->setURI($url)
                            ->takePhraseRequired();
 
-        if ($expected === 'false') 
-        {
+        if ($expected === 'false') {
             $this->assertFalse($slug);
-            return 1;
+            return 0;
         }
 
         $this->assertEquals($slug, $expected);
