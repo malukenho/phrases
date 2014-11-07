219b04ff0751341eb872e7142e7c1d07f3e4d475
malukenho <admin@phpse.net>
2014-11-07 08:13:17 -0300

    Documment the class

--- src/Phrases/Application.php
+++ src/Phrases/Application.php
@@ -3,23 +3,46 @@ namespace Phrases;
 
 class Application
 {
+    /**
+     * @var array
+     */
     private $phrases;
 
+    /**
+     * Constructor.
+     *
+     * @param array $phrases
+     */
     public function __construct(array $phrases)
     {
         $this->phrases = $phrases;
     }
 
+    /**
+     * Return all Phrase
+     *
+     * @return array
+     */
     public function getPhrases()
     {
         return $this->phrases;
     }
 
+    /**
+     * Return the single first phrase
+     *
+     * @return mixed
+     */
     public function getOnePhrase()
     {
     	return $this->phrases[0];
     }
 
+    /**
+     * Response to a Request
+     *
+     * @return void
+     */
     public function run()
     {
     	echo $this->getOnePhrase();
