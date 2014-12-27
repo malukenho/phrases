0fccaa234fd200ed8ec18029dc71f78dd400c1a4
Augusto Pascutti <augusto@phpsp.org.br>
2014-12-27 05:21:26 -0200

    Create MySQL persistance.
    Creates a RelationalTestCase classe used by SQLite and MySQL,
    allowing implementation of new strategies for relational databases.

--- tests/PhrasesTest/Persistance/AbstractRelationalTestCase.php
+++ tests/PhrasesTest/Persistance/AbstractRelationalTestCase.php
@@ -1,10 +1,10 @@
 <?php
 namespace Phrases\Persistance;
 
-use \Pdo;
+use Pdo;
 use PhrasesTestAsset\ConsumedData;
 
-class RelationalTest extends \PHPUnit_Framework_TestCase
+abstract class AbstractRelationalTestCase extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var \Pdo
@@ -18,31 +18,26 @@ class RelationalTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->pdo = new Pdo('sqlite::memory:');
-        $this->pdo
-             ->exec('
-                 CREATE TABLE phrases (
-                     id INTEGER PRIMARY KEY,
-                     title TEXT NOT NULL,
-                     text TEXT NOT NULL
-                 );
-             ');
-        $this->relational = new Relational($this->pdo);
+        $this->createPersistanceAdapter();
+        $this->assertNotEmpty($this->pdo, 'Please, set up a pdo instance on the test case.');
+        $this->assertNotEmpty($this->relational, 'Please, set up a relational adapter on the test case.');
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->relationalProvider = ConsumedData::asRelationalArray();
     }
 
+    abstract protected function createPersistanceAdapter();
+
     protected function tearDown()
     {
-        $this->pdo
-             ->exec('DROP TABLE phrases');
+        $this->pdo->exec('DROP TABLE phrases');
     }
 
     private function populatePhrasesTable(array $list)
     {
         $stm = $this->pdo
-                    ->prepare('INSERT INTO phrases(id, title, text) VALUES(:id, :title, :text);');
+                    ->prepare('INSERT INTO phrases(title, text) VALUES(:title, :text);');
 
-        $stm->bindValue(':id',    $list['id'],    Pdo::PARAM_INT);
+        //$stm->bindValue(':id',    $list['id'],    Pdo::PARAM_INT);
         $stm->bindValue(':title', $list['title'], Pdo::PARAM_STR);
         $stm->bindValue(':text',  $list['text'],  Pdo::PARAM_STR);
 
@@ -63,6 +58,23 @@ class RelationalTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    public function testSavePhraseReturnsInsertedId()
+    {
+        $phrase = [
+            'title' => 'New Phrase',
+            'text' => 'Hi, I am new!'
+        ];
+        $insertedId = $this->relational->save($phrase);
+        $this->assertEquals(
+            1,
+            $insertedId,
+            'Error inserting phrase.'
+        );
+    }
+
+    /**
+     * @depends testSavePhraseReturnsInsertedId
+     */
     public function testFindOneRandomWithOnlyOnePhraseInTheTableAlwaysReturnTheSamePhrase()
     {
         $expectedPhrase = $this->relationalProvider[0];
@@ -81,6 +93,9 @@ class RelationalTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * @depends testSavePhraseReturnsInsertedId
+     */
     public function testFindOneRandomWithPhrasesInTheTableReturnsFromTheList()
     {
         $list = $this->relationalProvider;
---
+++ tests/PhrasesTest/Persistance/MySQLTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phrases\Persistance;
+
+require_once 'AbstractRelationalTestCase.php';
+
+use Pdo;
+
+class MySQLTest extends AbstractRelationalTestCase
+{
+    /**
+     * @TODO Remove database provisioning from here.
+     * @TODO Move database connection options to environment variables.
+     */
+    protected function createPersistanceAdapter()
+    {
+        $username = 'root';
+        $password = (getenv('CONTINOUS_INTEGRATION') == 'true') ? '' : 'root';
+        $pdo = new Pdo('mysql:host=localhost', $username, $password);
+        $pdo->exec('CREATE DATABASE IF NOT EXISTS phrases_test');
+        unset($pdo);
+
+        $this->pdo = new Pdo('mysql:host=localhost;dbname=phrases_test', $username, $password);
+        $this->relational = new MySQL($this->pdo);
+        $this->relational->createTables();
+    }
+}
