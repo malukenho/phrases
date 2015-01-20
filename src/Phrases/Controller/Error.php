409ba39f6828c52dc37b1b499d7e9e63ea05a8a6
malukenho <admin@phpse.net>
2015-01-19 21:09:23 -0300

    Update Error Controller

--- src/Phrases/Controller/Error.php
+++ src/Phrases/Controller/Error.php
@@ -6,14 +6,33 @@ use Zend\Http;
 
 class Error implements ExecutionInterface
 {
+    /**
+     * @var integer represents a http status code
+     */
     private $statusCode;
+
+    /**
+     * @var string represents a message of http response
+     */
     private $statusMessage;
 
+    /**
+     * Constructor.
+     *
+     * @param integer $statusCode
+     * @param string  $statusMessage
+     */
     public function __construct($statusCode, $statusMessage)
     {
         $this->statusMessage = $statusMessage;
         $this->statusCode = $statusCode;
     }
+
+    /**
+     * @param Http\Request $request
+     *
+     * @return Zend\Http\Response|Http\Response
+     */
     public function execute(Http\Request $request)
     {
         $response = new Http\Response();
