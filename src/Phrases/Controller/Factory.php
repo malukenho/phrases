a653459495b6f4f185afa27ab0ddebe2b97a98cd
malukenho <admin@phpse.net>
2015-01-19 21:50:52 -0300

    Add post to factory

--- src/Phrases/Controller/Factory.php
+++ src/Phrases/Controller/Factory.php
@@ -20,6 +20,13 @@ class Factory
             return new GetPhrase($this->constructorArgs);
         }
 
+        if ($request->isPost()) {
+            return new PostPhrase(
+                $this->constructorArgs,
+                $request->getPost()->toArray()
+            );
+        }
+
         return new Error(405, 'Method not allowed');
     }
 }
